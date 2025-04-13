25a36701a49643bbf34c00170d944cc8f94aa27f
Rodrigo <rodrigosennasantos@hotmail.com>
2025-04-13 20:06:20 -0300

    Criando projeto no repositorio

---
+++ src/main/java/financialTech/FinancialTechApplication.java
@@ -0,0 +1,13 @@
+package financialTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/financialTech/FinancialTechApplicationTests.java
@@ -0,0 +1,13 @@
+package financialTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
