74f66d2d8841960b7aeb9b9fae215ac91d8c37e1
Rodrigo <rodrigosennasantos@hotmail.com>
2025-04-15 21:25:18 -0300

    Criando service de lojista

---
+++ src/main/java/financialTech/Services/LojistaService.java
@@ -0,0 +1,28 @@
+package Services;
+
+import Usuarios.Lojista;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service
+public class LojistaService {
+
+    private List<Lojista> lojistas = new ArrayList<>();
+
+    public Lojista createLojista(Lojista lojista) {
+        for(Lojista l : lojistas) {
+            if(l.getEmail().equalsIgnoreCase(lojista.getEmail())) {
+                throw new RuntimeException("Email já cadastrado!");
+            }
+        }
+
+        for(Lojista l : lojistas) {
+            if(l.getCnpj().equals(lojista.getCnpj())){
+                throw new RuntimeException("CNPJ já existe");
+            }
+        }
+        lojistas.add(lojista);
+        return lojista;
+    }
+}
